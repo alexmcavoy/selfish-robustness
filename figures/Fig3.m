f6a05b47f81f8fa2f079723515ea6fc4f0a2a3ea
Alex McAvoy <alexmcavoy@gmail.com>
2021-08-10 17:32:53 -0400

    fix typo in game parameters

--- figures/Fig3.m
+++ figures/Fig3.m
@@ -7,9 +7,6 @@ if ~exist('output', 'dir')
     mkdir output;
 end
 
-% prisoner's dilemma with (S+T)/2 > R
-game_parameters = [2, -1, 7, 0];
-
 % number of initial strategies to sample
 initial_conditions = 1e5;
 
@@ -19,22 +16,28 @@ error_probability = 0;
 % learning rate
 learning_rate = 1e-2;
 
+% game parameters for underlying one-shot games
+one_shot_games = [2, -1, 7, 0;
+    4, 0, 5, 3];
+
 % fixed strategies for player X
 fixed_strategies = [1, 0.12, 0.88, 0;
     1, 0.85, 0.15, 0];
 
+payoff_increments = [2, 1];
+
 % build panels a and b
 panels = ['a', 'b'];
 for panel=1:length(panels)
+    game_parameters = one_shot_games(panel, :);
     % fixed strategy for X
     p = fixed_strategies(panel, :);
     %
     piX_final = zeros(1, initial_conditions);
     piY_final = zeros(1, initial_conditions);
     parfor sample=1:initial_conditions
-        disp(sample);
         [~, piY_final(sample), piX_final(sample)] = optimize(random('beta', 0.5, 0.5, 1, 4), p, game_parameters, error_probability, learning_rate, -1, 0);
     end
     payoffs = [piX_final; piY_final];
-    print_heatmap(payoffs, [], game_parameters, 2, strcat('output/Fig3', panels(panel)));
+    print_heatmap(payoffs, [], game_parameters, payoff_increments(panel), strcat('output/Fig3', panels(panel)));
 end
